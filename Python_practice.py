367bbf20ce9727e867acf75e1a02339b01b483ec
ROY8181 <156818057+ROY8181@users.noreply.github.com>
2024-01-31 15:49:29 +0000

    Update Python_practice.py

--- Python_practice.py
+++ Python_practice.py
@@ -11,4 +11,26 @@ if __name__ == '__main__':
                 if i+j+k != n:
                     coordinate.append([i,j,k])
     print(coordinate)
+# find run-up score
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    x = list(arr)
+    x.sort()
+print(x[2])
 
+#leap year
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+            
+    
+    return leap

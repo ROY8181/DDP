14035bcccfa243878e8b5979a95b3cd80b0acc30
ROY8181 <156818057+ROY8181@users.noreply.github.com>
2024-01-31 15:49:14 +0000

    Create Python_practice.py

---
+++ Python_practice.py
@@ -0,0 +1,14 @@
+# list comprehension 
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    coordinate = []
+    for i in range(0,x+1):
+        for j in range(0, y+1):
+            for k in range(0, z+1):
+                if i+j+k != n:
+                    coordinate.append([i,j,k])
+    print(coordinate)
+
